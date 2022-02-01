5c4b5482e629f3f04c5b5cf2e7e87d99ba5c880a
ThamerAyachi <thamerjak3@gmail.com>
2022-02-01 18:48:50 +0100

    beta

---
+++ src/main/java/io/rasputin/springsecurityjwt/models/AuthenticationRequest.java
@@ -0,0 +1,11 @@
+package io.rasputin.springsecurityjwt.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
